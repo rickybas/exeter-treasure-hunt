7ea721d7fa2c6ee7302aea3e92c9ad83c8ae814f
rickybas <rickybas12@gmail.com>
2020-02-12 00:44:32 +0000

    add bootstrap #24

--- app/exeter_treasure_hunt.py
+++ app/exeter_treasure_hunt.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 APP_NAME = "ExePlore"
 VERSION = "0.1"
 
 app = Flask(__name__)
+Bootstrap(app)
 
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template("index.html", APP_NAME=APP_NAME)
+    return render_template("index.html", APP_NAME=APP_NAME, VERSION=VERSION)
