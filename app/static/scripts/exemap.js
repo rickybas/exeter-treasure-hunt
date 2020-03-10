499c9f03a344316ebf228ab210062b6fb10999dc
Leon Cresdee <leon@cresdee.com>
2020-03-10 14:56:07 +0000

    Added start area
    - Circle surrounding start area has been added
    - Also coloured other players

--- app/static/scripts/exemap.js
+++ app/static/scripts/exemap.js
@@ -1,11 +1,15 @@
 /*jslint es60*/
+//API key
 mapboxgl.accessToken = "pk.eyJ1IjoibGNyZXNkZWUiLCJhIjoiY2s3MTNnbXU3MDJycjNkczF1OHR2YjdmNCJ9.6eD1kNQOyOGLeiGr6udiZw";
 
+
+//Map bounds
 var bounds = [
     [-3.540540, 50.730483],     //SW coords
     [-3.528044, 50.741516]//NE coords
 ];
 
+//Initiates the map
 var mapcontainer = document.getElementById("game-map");
 var map = new mapboxgl.Map({
     container: "map",
@@ -20,6 +24,7 @@ var map = new mapboxgl.Map({
 
 //[longitude, latitude]
 var playerpos = [0, 0];
+
 
 function setPlayerPos(position){
     playerpos[1] = position.coords.latitude;
@@ -38,8 +43,8 @@ function updateGeoLocation(){
     }
 }
 
-
 updateGeoLocation();
+
 var playerGeo = {
     type: 'Feature',
     geometry: {
@@ -51,15 +56,17 @@ var playerGeo = {
     }
 };
 
-
-
+//Adds player locater GUI element
 map.addControl(new mapboxgl.GeolocateControl({
     positionOptions: {
         enableHighAccuracy: true
     },
     trackUserLocation: true
-}))
+}));
 
+const metersToPixelAtMaxZoom = (meters, latitude) => meters / 0.075 / Math.cos(latitude * Math.PI / 180)
+
+//Called when map loads
 map.on('load', function(){
     map.addSource('player', {
         'type': 'geojson',
@@ -89,10 +96,42 @@ map.on('load', function(){
         'type': 'circle',
         'source': 'others',
         'paint': {
-            'circle-radius': 6    
+            'circle-radius': {
+                stops: [[0,6], [20, 20]],
+                base: 2
+            }, 
+            'circle-color': '#0783ff'
+        }
+    });
+    map.addSource('start-zone', {
+        'type': 'geojson',
+        'data': {
+            'type': 'Feature',
+            'geometry':{
+                'type': 'Point',
+                'coordinates': [-3.534516, 50.735770]
+            }
+        }
+    });
+    map.addLayer({
+        'id': 'start-zone',
+        'source': 'start-zone',
+        'type': 'circle',
+        'paint': {
+            'circle-color': '#52d1ff',
+            'circle-opacity': 0.4,
+            'circle-stroke-color': '#0a7aa3',
+            'circle-stroke-width': 2,
+            'circle-radius': {
+                stops: [[0,0],
+                        [20, metersToPixelAtMaxZoom(30, -3.534516)]],
+                base: 2
+            }
         }
     });
 });
+
+
 
 map.flyTo({
     center: playerpos
